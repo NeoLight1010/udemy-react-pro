b92c5e24e7538ca9870645f9c03099ba56c14e84
Anthony Suárez <neolight1010@gmail.com>
2022-03-08 11:48:19 -0500

    Implment lazy-loading with nested routes.

---
+++ react-app/src/01-lazyload/pages/NoLazy.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const NoLazy: React.FC = () => {
+    return (
+        <>
+            <h1>Not lazy page</h1>
+        </>
+    )
+}
--- react-app/src/routes/routes.ts
+++ react-app/src/routes/routes.ts
@@ -1,4 +1,5 @@
 import React, { lazy } from "react";
+import {NoLazy} from "../01-lazyload/pages/NoLazy";
 
 interface Route {
     to: string;
@@ -7,42 +8,19 @@ interface Route {
     Component: React.ComponentType;
 }
 
-const LazyPage1 = lazy(
-    () =>
-        import(
-            /* webpackChunkName: "LazyPage1" */ "../01-lazyload/pages/LazyPage1"
-        )
-);
-const LazyPage2 = lazy(
-    () =>
-        import(
-            /* webpackChunkName: "LazyPage2" */ "../01-lazyload/pages/LazyPage2"
-        )
-);
-const LazyPage3 = lazy(
-    () =>
-        import(
-            /* webpackChunkName: "LazyPage3" */ "../01-lazyload/pages/LazyPage3"
-        )
-);
+const LazyLayout = lazy(() =>  import("../01-lazyload/layout/LazyLayout"));
 
 export const routes: Route[] = [
     {
-        to: "/lazy1",
-        path: "lazy1",
-        Component: LazyPage1,
-        name: "Lazy-1",
+        to: "/lazyload/",
+        path: "lazyload/*",
+        Component: LazyLayout,
+        name: "Lazy Layout",
     },
     {
-        to: "/lazy2",
-        path: "lazy2",
-        Component: LazyPage2,
-        name: "Lazy-2",
-    },
-    {
-        to: "/lazy3",
-        path: "lazy3",
-        Component: LazyPage3,
-        name: "Lazy-3",
+        to: "/no-lazy",
+        path: "no-lazy",
+        Component: NoLazy,
+        name: "No Lazy",
     },
 ];

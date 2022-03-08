19e227f032ba6762f1780aed8a888671f411f497
Anthony Suárez <neolight1010@gmail.com>
2022-03-08 12:48:54 -0500

    Refactor ProductCard with classic design: component-patterns.

--- react-app/src/component-patterns/components/ProductCard.tsx
+++ react-app/src/component-patterns/components/ProductCard.tsx
@@ -1,19 +1,29 @@
 import styles from "../styles/styles.module.css";
+import noImage from "../assets/no-image.jpg";
 import React from "react";
 import {useProduct} from "../hooks/useProduct";
 
-export const ProductCard: React.FC = () => {
+interface ProductCardProps {
+    product: Product;
+}
+
+interface Product {
+    title: string;
+    img?: string;
+}
+
+export const ProductCard: React.FC<ProductCardProps> = ({product}) => {
     const { counter, increaseBy } = useProduct();
 
     return (
         <div className={styles.productCard}>
             <img
                 className={styles.productImg}
-                src={"./coffee-mug.png"}
-                alt="Coffee Mug"
+                src={product.img || noImage}
+                alt={product.title}
             />
 
-            <span className={styles.productDescription}>Coffee Mug</span>
+            <span className={styles.productDescription}>{product.title}</span>
 
             <div className={styles.buttonsContainer}>
                 <button
--- react-app/src/component-patterns/pages/ShoppingPage.tsx
+++ react-app/src/component-patterns/pages/ShoppingPage.tsx
@@ -1,12 +1,26 @@
 import React from "react";
-import {ProductCard} from "../components/ProductCard";
+import { ProductCard } from "../components/ProductCard";
 
 export const ShoppingPage: React.FC = () => {
     return (
         <div>
             <h1>Shopping Store</h1>
             <hr />
-            <ProductCard />
+
+            <div
+                style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    flexWrap: "wrap",
+                }}
+            >
+                <ProductCard
+                    product={{
+                        title: "Coffe Mug",
+                        img: "./coffee-mug.png",
+                    }}
+                />
+            </div>
         </div>
     );
 };

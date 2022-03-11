053dd8f3a6ca1b0fd22e0637e4794f7f95f31fea
Anthony Suárez <neolight1010@gmail.com>
2022-03-11 17:43:59 -0500

    Create PWA with Workbox.

--- react-calendar-frontend/src/index.js
+++ react-calendar-frontend/src/index.js
@@ -1,12 +1,6 @@
-import ReactDOM from 'react-dom';
-import { CalendarApp } from './CalendarApp';
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import ReactDOM from "react-dom";
+import { CalendarApp } from "./CalendarApp";
 
-import './styles.css';
+import "./styles.css";
 
-ReactDOM.render(
-    <CalendarApp />,
-  document.getElementById('root')
-);
-
-serviceWorkerRegistration.register();
+ReactDOM.render(<CalendarApp />, document.getElementById("root"));
---
+++ react-calendar-frontend/src/sw-template.js
@@ -0,0 +1,63 @@
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js"
+);
+
+workbox.loadModule("workbox-background-sync");
+
+workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+
+const { registerRoute } = workbox.routing;
+const { CacheFirst, NetworkFirst, NetworkOnly } = workbox.strategies;
+const { BackgroundSyncPlugin } = workbox.backgroundSync;
+
+const networkFirstRoutes = ["/api/auth/renew", "/api/events"];
+
+registerRoute(({ req, url }) => {
+  if (networkFirstRoutes.includes(url.pathname)) {
+    return true;
+  }
+
+  return false;
+}, new NetworkFirst());
+
+const cacheFirstRoutes = [
+  "https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css",
+  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css",
+];
+
+registerRoute(({ req, url }) => {
+  if (cacheFirstRoutes.includes(url.href)) {
+    return true;
+  }
+
+  return false;
+}, new CacheFirst());
+
+// Offline posts
+const bgSyncPlugin = new BackgroundSyncPlugin("offline-posts", {
+  maxRetentionTime: 24 * 60,
+});
+
+registerRoute(
+  new RegExp("http://localhost:4000/api/events"),
+  new NetworkOnly({
+    plugins: [bgSyncPlugin],
+  }),
+  "POST"
+);
+
+registerRoute(
+  new RegExp("http://localhost:4000/api/events/"),
+  new NetworkOnly({
+    plugins: [bgSyncPlugin],
+  }),
+  "PUT"
+);
+
+registerRoute(
+  new RegExp("http://localhost:4000/api/events/"),
+  new NetworkOnly({
+    plugins: [bgSyncPlugin],
+  }),
+  "DELETE"
+);

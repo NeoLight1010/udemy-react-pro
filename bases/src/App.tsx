0efc9ea8c30b38a2ad5a02043145e479f3a88877
Anthony Suárez <neolight1010@gmail.com>
2022-03-11 10:30:02 -0500

    Use neolight-storybook-components: bases.

--- bases/src/App.tsx
+++ bases/src/App.tsx
@@ -1,20 +1,34 @@
-import {Counter} from "./bases/Counter";
-import {CounterBy} from "./bases/CounterBy";
-import {CounterEffect} from "./bases/CounterEffect";
-import {CounterHook} from "./bases/CounterHook";
-import {CounterReducer} from "./counter-reducer/CounterReducer";
+import { MyLabel } from "neolight-storybook-components";
+import { Counter } from "./bases/Counter";
+import { CounterBy } from "./bases/CounterBy";
+import { CounterEffect } from "./bases/CounterEffect";
+import { CounterHook } from "./bases/CounterHook";
+import { CounterReducer } from "./counter-reducer/CounterReducer";
 
 function App() {
-  return (
-    <>
-      <Counter initialValue={15} />
-      <CounterBy />
-      <CounterEffect initialValue={9} />
-      <CounterEffect initialValue={8} />
-      <CounterHook initialValue={8} />
-      <CounterReducer />
-    </>
-  );
+    return (
+        <div>
+            <h1>Hello World!</h1>
+
+            <br />
+
+            <MyLabel label="Welcome to my React project!" backgroundColor="white" />
+
+            <hr />
+
+            <Counter initialValue={15} />
+            <br />
+            <CounterBy />
+            <br />
+            <CounterEffect initialValue={9} />
+            <br />
+            <CounterEffect initialValue={8} />
+            <br />
+            <CounterHook initialValue={8} />
+            <br />
+            <CounterReducer />
+        </div>
+    );
 }
 
 export default App;
